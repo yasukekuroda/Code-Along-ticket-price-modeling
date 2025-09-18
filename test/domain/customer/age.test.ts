684e2d8b1a74a71ca49f97d6f968265ac2924d5f
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-18 23:57:31 +0900

    Ageクラスとテストコードを追加

---
+++ test/domain/customer/age.test.ts
@@ -0,0 +1,22 @@
+import { Age } from "domain/customer/age";
+
+describe("Age", () => {
+  describe("new", () => {
+    test("小数値が与えられた場合、エラーになる", () => {
+      expect(() => new Age(25.5)).toThrow("年齢が不正です");
+    });
+
+    test("0未満の整数が与えられた場合、エラーになる", () => {
+      expect(() => new Age(-1)).toThrow("年齢が不正です");
+    });
+
+    test("201以上の整数が与えられた場合、エラーになる", () => {
+      expect(() => new Age(201)).toThrow("年齢が不正です");
+    });
+
+    test("0以上200以下の整数が与えられた場合、エラーにならない", () => {
+      expect(new Age(0)).toBeInstanceOf(Age);
+      expect(new Age(200)).toBeInstanceOf(Age);
+    });
+  });
+});
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,8 @@
-import { add } from "index";
+// import { add } from "index";
 
-test('2+3は5になる.', () => {
-  expect(add(2, 3)).toBe(5);
+// test('2+3は5になる.', () => {
+//   expect(add(2, 3)).toBe(5);
+// });
+test("2+3は5になる.", () => {
+  expect(5).toBe(5);
 });
