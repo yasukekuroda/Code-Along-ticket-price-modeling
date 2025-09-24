b0f74866ad17ab449b9e29424db958581bee28cb
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-24 21:14:43 +0900

    format

--- test/domain/customer/customer.test.ts
+++ test/domain/customer/customer.test.ts
@@ -1,17 +1,13 @@
 import { Age, Customer } from "domain/customer";
 
 describe("Customer", () => {
-    describe("new", () => {
-        test("Ageに不正な値が与えられた場合、インスタンス生成に失敗する", () => {
-            expect(() => new Customer(
-                new Age(-1),
-            )).toThrow("年齢が不正です");
-        });
-
-        test("Ageに正しい値が与えられた場合、インスタンス生成に成功する", () => {
-            expect(new Customer(
-                new Age(25),
-            )).toBeInstanceOf(Customer);
-        });
+  describe("new", () => {
+    test("Ageに不正な値が与えられた場合、インスタンス生成に失敗する", () => {
+      expect(() => new Customer(new Age(-1))).toThrow("年齢が不正です");
     });
+
+    test("Ageに正しい値が与えられた場合、インスタンス生成に成功する", () => {
+      expect(new Customer(new Age(25))).toBeInstanceOf(Customer);
+    });
+  });
 });
