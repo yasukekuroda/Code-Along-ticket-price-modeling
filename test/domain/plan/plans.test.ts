c82c6d9b44b78a9677ff2f4149f7a7dd569ccb6c
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-24 22:41:04 +0900

    feat: Plansクラスのテストを追加し、countおよびavailablePlansメソッドの動作を確認するテストケースを実装しました

--- test/domain/plan/plans.test.ts
+++ test/domain/plan/plans.test.ts
@@ -1,9 +1,30 @@
-import { Plans } from "domain/plan";
+import { Age, Customer } from "domain/customer";
+import { Plan, Plans, allPlans } from "domain/plan";
 
 describe("Plans", () => {
-  describe("new", () => {
+  describe("count", () => {
     test("plansが空の場合は0を返す", () => {
       expect(new Plans([]).count()).toBe(0);
+    });
+
+    test("plansが空ではない場合はplansの数を返す", () => {
+      expect(new Plans([{} as Plan]).count()).toBe(1);
+    });
+  });
+
+  describe("availablePlans", () => {
+    const customer = new Customer(
+      new Age(30),
+    );
+
+    test("plansが空の場合は0を返す", () => {
+      expect(new Plans([]).availablePlans(customer).count()).toBe(0);
+    });
+
+    test("plansが空ではない場合は利用可能なplansの数を返す", () => {
+      const availablePlans = allPlans.availablePlans(customer);
+      expect(availablePlans.count()).toBe(0);
+      expect(availablePlans.availablePlans(customer).count()).toBe(0);
     });
   });
 });
