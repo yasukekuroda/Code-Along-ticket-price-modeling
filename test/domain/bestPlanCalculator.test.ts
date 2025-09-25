f06846178fc50026c94c58b212face5b715e5702
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-25 09:08:03 +0900

    feat: BestPlanCalculatorのテストを追加し、基本的な計算機能を確認するテストケースを実装しました

---
+++ test/domain/bestPlanCalculator.test.ts
@@ -0,0 +1,8 @@
+import { Age } from "domain/customer";
+import { Customer } from "domain/customer";
+import { CinemaDate } from "domain/date";
+import { BestPlanCalculator } from "domain/bestPlanCalculator";
+
+test("2 + 3は5になる", () => {
+  expect(2 + 3).toBe(5);
+});
